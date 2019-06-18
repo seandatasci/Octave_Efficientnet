197218258b231412fe336977df51e76d406d8204
Sean <sean_abreau@brown.edu>
2019-06-18 08:15:45 -0700

    Update efficientoctave_model.py

--- efficientoctave/efficientoctave_model.py
+++ efficientoctave/efficientoctave_model.py
@@ -1,4 +1,4 @@
-# Copyright 2019 Sean Abreau and Antony Sagayaraj. All Rights Reserved.
+# Copyright 2019 seandatasci and Antony Sagayaraj. All Rights Reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
