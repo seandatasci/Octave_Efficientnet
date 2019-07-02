73f004534aad106a4c5e659b485e70ca5e3f35c9
Sean <sean_abreau@brown.edu>
2019-07-01 18:13:45 -0700

    Update efficientoctave_model.py

--- efficientoctave-tpu/efficientoctave_model.py
+++ efficientoctave-tpu/efficientoctave_model.py
@@ -208,8 +208,8 @@ GlobalParams = collections.namedtuple('GlobalParams', [
 ])
 GlobalParams.__new__.__defaults__ = (None,) * len(GlobalParams._fields)
 
-batchnorm = tf.layers.BatchNormalization
-# batchnorm = utils.TpuBatchNormalization  # TPU-specific requirement.
+#batchnorm = tf.layers.BatchNormalization
+batchnorm = utils.TpuBatchNormalization  # TPU-specific requirement.
 
 
 BlockArgs = collections.namedtuple('BlockArgs', [
